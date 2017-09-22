6eddd373500b485448bff20800b8088ce0eef3f1
Ravikiran <kienme@gmail.com>
2017-09-23 00:32:23 +0530

    Store cookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {ADD_REMINDER, DELETE_REMINDER, COMPLETE_REMINDER} from '../constants';
+import {bake_cookie, read_cookie} from 'sfcookies';
 
 //Helper functions
 
@@ -13,20 +14,25 @@ const reminder = (action) => {
 //Reducer to export
 
 const reminders = (state=[], action) => {
-	let reminders = null;	
+	let reminders = null;
+	state = read_cookie('reminders');
+
 	switch(action.type) {
 		case ADD_REMINDER:
 			reminders = [...state, reminder(action)];
+			bake_cookie('reminders', reminders);
 			return reminders;
 
 		case DELETE_REMINDER:
 			reminders = state.filter(s => s.id !== action.id);
+			bake_cookie('reminders', reminders);
 			return reminders;
 
 		case COMPLETE_REMINDER:
 			reminders = state.slice();	//copy
 			const index = reminders.findIndex(r => r.id===action.id);
 			reminders[index].completed = action.completed;
+			bake_cookie('reminders', reminders);
 			return reminders;
 		
 		default:
